76db3cdab402670aa01cda37424f52b429ff2be7
Harvey Chen <harveyc95@gmail.com>
2017-11-08 22:55:32 -0500

    added boundingBoxTest

--- OpenCV/testcase.py
+++ OpenCV/testcase.py
@@ -3,19 +3,19 @@ import numpy as np
 import imutils
 
 class Box:
-	xMin = 0
-	xMax = 0
-	yMin = 0
-	yMax = 0
+	_xMin = 0
+	_xMax = 0
+	_yMin = 0
+	_yMax = 0
 
 	def __init__(self, xMin, yMin, xMax, yMax):
-		self.xMin = xMin
-		self.xMax = xMax
-		self.yMin = yMin
-		self.yMax = yMax
+		self._xMin = xMin
+		self._xMax = xMax
+		self._yMin = yMin
+		self._yMax = yMax
 
 	def __repr__(self):
-		return "(" + str(self.xMin) + ", " + str(self.yMin) + ")" + "(" + str(self.xMax) + ", " + str(self.yMax) + ")"
+		return "(" + str(self._xMin) + ", " + str(self._yMin) + ")" + "(" + str(self._xMax) + ", " + str(self._yMax) + ")"
 
 # Input: image
 # Output: bounding box of xMin, yMin, xMax, yMax
@@ -41,10 +41,19 @@ def boundingBox (image):
 	print box
 	return box
 
+def boudingTest(image, box):
+	for col in range (box._xMin, box._xMax, 1):
+		image[box._yMin][col] = [255,255,255]
+		image[box._yMax][col] = [255,255,255]
+	for row in range (box._yMin, box._yMax, 1):
+		image[row][box._xMin] = [255,255,255]
+		image[row][box._xMax] = [255,255,255]
+	return image
+
 # Input: image, filename, extension, scaling, save
 # Output: 
 # Note: 
-def resize(image, filename, extension, scaling, save):
+def resize(image, filename, extension, scaling):
 	width = image.shape[1]
 	height = image.shape[0]
 
@@ -52,22 +61,29 @@ def resize(image, filename, extension, scaling, save):
 	heightResized = int(height*scaling)
 	resized = cv2.resize(image, (widthResized,heightResized))
 	resizedName = filename + '_' + str(scaling) + 'x_' + str(widthResized) + 'x' + str(heightResized) + extension
-	if (save == True):
-		cv2.imwrite(resizedName, resized)
-
 	return resized
+
+def rotate(image, filename, extension, start, end, step):
+	for angle in np.arange(start, end+1, step):
+		rotated = imutils.rotate_bound(image, angle)
+		dst = filename + '_' + str(angle) + extension
+		# cv2.imwrite(dst, rotated)
+		box = boundingBox(rotated)
+		test = boudingTest(rotated, box)
+		cv2.imwrite(dst, test)
 
 def createData (filename, extension, scaling, save):
 	image = cv2.imread(filename+extension)
-	resized = resize(image, filename, extension, scaling, save)
-	boundingBox(resized)
+	resized = resize(image, filename, extension, scaling)
+	rotate(resized, filename, extension, 0, 360, 30)
+	# box = boundingBox(resized)
 	pass
 
 fileArm = 'A_Arm'
 fileHand = 'A_Hand'
 fileTip = 'A_Tip'
 ext = '.png'
-scaling = 0.65
+scaling = 0.25
 background = 'test.jpg'
 save = True
 numTestCases = 100
