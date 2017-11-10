308d159d79dbd7fb5b520944a3f3692becbd9e93
Harvey Chen <harveyc95@gmail.com>
2017-11-10 13:36:36 -0500

    implemented overlay, and MyImage class

--- OpenCV/testcase.py
+++ OpenCV/testcase.py
@@ -17,29 +17,107 @@ class Box:
 	def __repr__(self):
 		return "(" + str(self._xMin) + ", " + str(self._yMin) + ")" + "(" + str(self._xMax) + ", " + str(self._yMax) + ")"
 
-# Input: image
-# Output: bounding box of xMin, yMin, xMax, yMax
-# Note: assumes entire image is black except the part looking for bounding box
-def boundingBox (image):
-	w = image.shape[1]
-	h = image.shape[0]
-	xMin = w
-	xMax = 0
-	yMin = h
-	yMax = 0
-	black = np.zeros((1,1,3), np.uint8)
-	for col in range (0, w):
-		for row in range (0, h):
-			if (np.any(image[row][col] != black)):
-				xMin = min(xMin, col)
-				xMax = max(xMax, col)
-				yMin = min(yMin, row)
-				yMax = max(yMax, row)
-				image[row][col] = [255,255,255]
-
+def adjust(xMin, yMin, xMax, yMax, x, y):
+	xMin = x - xMin
+	yMin = y - yMin
+	xMax = xMax - xMin
+	yMax = yMax - yMin
 	box = Box(xMin, yMin, xMax, yMax)
 	print box
 	return box
+
+class MyImage:
+	def __init__(self, img_name, img_ext):
+		self.img = cv2.imread(img_name+img_ext)
+		self.name = img_name
+		self.ext = img_ext
+		self.box = None
+
+	def __str__(self):
+		return self.name
+
+	def show(self):
+		cv2.imshow(self.name, self.img)
+		cv2.waitKey(0)
+
+	def resize(self, scaling):
+		width = self.img.shape[1]
+		height = self.img.shape[0]
+		widthResized = int(width*scaling)
+		heightResized = int(height*scaling)
+		resized = cv2.resize(self.img, (widthResized,heightResized))
+		resizedName = self.name + '_' + str(scaling) + 'x_' + str(widthResized) + 'x' + str(heightResized) + self.ext
+		self.img = resized
+		self.name = resizedName
+
+	def rotate(self, angle):
+		rotated = imutils.rotate_bound(self.img, angle)
+		rotatedName = self.name + '_' + str(angle) + self.ext
+		self.img = rotated
+		self.name = rotatedName
+
+	def boundingBox(self):
+		w = self.img.shape[1]
+		h = self.img.shape[0]
+		xMin = w
+		xMax = 0
+		yMin = h
+		yMax = 0
+		black = np.zeros((1,1,3), np.uint8)
+		for col in range (0, w):
+			for row in range (0, h):
+				if (np.any(self.img[row][col] != black)):
+					xMin = min(xMin, col)
+					xMax = max(xMax, col)
+					yMin = min(yMin, row)
+					yMax = max(yMax, row)
+
+		box = Box(xMin, yMin, xMax, yMax)
+		self.box = box
+
+	def overlay(self, background, xDesired, yDesired, xTip, yTip):
+		black = np.zeros((1080,1920,3), np.uint8)
+		w = self.img.shape[1]
+		h = self.img.shape[0]
+		xOffset = xDesired - xTip
+		yOffset = yDesired - yTip
+		xBgStart = max(0, 0 + xOffset)
+		yBgStart = max(0, 0 + yOffset)
+		xBgEnd = min(1920, w + xOffset)
+		yBgEnd = min(1080, h + yOffset)
+
+		print xBgEnd-xBgStart, yBgEnd-yBgStart
+
+		bgCrop = background.img[yBgStart:yBgEnd, xBgStart:xBgEnd]
+		# cv2.imshow('bgCrop', bgCrop)
+		
+		xFgStart = xBgStart - xOffset
+		yFgStart = yBgStart - yOffset
+		xFgEnd = xBgEnd - xOffset
+		yFgEnd = yBgEnd - yOffset
+
+		print xFgEnd-xFgStart, yFgEnd-yFgStart
+
+		fgCrop = self.img[yFgStart:yFgEnd, xFgStart:xFgEnd]
+
+		# Now create a mask and create its inverse
+		fgImgGray = cv2.cvtColor(fgCrop,cv2.COLOR_BGR2GRAY)
+		ret, mask = cv2.threshold(fgImgGray, 10, 255, cv2.THRESH_BINARY)
+		mask_inv = cv2.bitwise_not(mask)
+
+		# Now black-out the area of logo in ROI
+		bgCropMasked = cv2.bitwise_and(bgCrop,bgCrop, mask = mask_inv)
+
+		# Put logo in ROI and modify the main image
+		overlayed = cv2.add(bgCropMasked,fgCrop)
+		result = np.copy(background.img)
+		result[yBgStart:yBgEnd, xBgStart:xBgEnd] = overlayed
+		cv2.imshow('res',result)
+		cv2.waitKey(0)
+
+# Input: image
+# Output: bounding box of xMin, yMin, xMax, yMax
+# Note: assumes entire image is black except the part looking for bounding box
 
 def boudingTest(image, box):
 	for col in range (box._xMin, box._xMax, 1):
@@ -50,42 +128,51 @@ def boudingTest(image, box):
 		image[row][box._xMax] = [255,255,255]
 	return image
 
-# Input: image, filename, extension, scaling, save
-# Output: 
-# Note: 
-def resize(image, filename, extension, scaling):
-	width = image.shape[1]
-	height = image.shape[0]
 
-	widthResized = int(width*scaling)
-	heightResized = int(height*scaling)
-	resized = cv2.resize(image, (widthResized,heightResized))
-	resizedName = filename + '_' + str(scaling) + 'x_' + str(widthResized) + 'x' + str(heightResized) + extension
-	return resized
+# def overlay (bgImg, fgImg, x, y):
+# 	fgBox = adjust(0,0,fgImg.shape[1],fgImg.shape[0], x, y)
 
-def rotate(image, filename, extension, start, end, step):
-	for angle in np.arange(start, end+1, step):
-		rotated = imutils.rotate_bound(image, angle)
-		dst = filename + '_' + str(angle) + extension
-		# cv2.imwrite(dst, rotated)
-		box = boundingBox(rotated)
-		test = boudingTest(rotated, box)
-		cv2.imwrite(dst, test)
+# 	xStart = 0 if (fgBox._xMin < 0) else fgBox._xMin
+# 	yStart = 0 if (fgBox._yMin < 0) else fgBox._yMin
 
-def createData (filename, extension, scaling, save):
-	image = cv2.imread(filename+extension)
-	resized = resize(image, filename, extension, scaling)
-	rotate(resized, filename, extension, 0, 360, 30)
-	# box = boundingBox(resized)
-	pass
+def createData (arm, hand, tip, background, scaling, angle):
+	arm.resize(scaling)
+	arm.rotate(angle)
+
+	hand.resize(scaling)
+	hand.rotate(angle)
+
+	tip.resize(scaling)
+	tip.rotate(angle)
+	tip.boundingBox()
+	fgBox = tip.box
+	xFg = ((fgBox._xMin + fgBox._xMax)/2)
+	yFg = ((fgBox._yMin + fgBox._yMax)/2)
+	print xFg, yFg
+	xDesired = 200
+	yDesired = 600
+
+	print "overlay"
+
+	for i in range (10, 1000, 200):
+		for j in range (10, 1000, 200):
+			arm.overlay(background, i, j, xFg, yFg)
 
 fileArm = 'A_Arm'
 fileHand = 'A_Hand'
 fileTip = 'A_Tip'
 ext = '.png'
-scaling = 0.25
-background = 'test.jpg'
-save = True
+scaling = 0.75
+angle = 0
+background = 'test'
 numTestCases = 100
 
-createData(fileHand, ext, scaling, save)
+arm = MyImage(fileArm,ext)
+hand = MyImage(fileHand,ext)
+tip = MyImage(fileTip,ext)
+background = MyImage(background,'.jpg')
+
+createData(arm, hand, tip, background, scaling, angle)
+
+
+
